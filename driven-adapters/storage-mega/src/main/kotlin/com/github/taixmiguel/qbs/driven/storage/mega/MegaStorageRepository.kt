b643ec16a69f56827d911753d3eae9c3d286fca3
TaixMiguel <taixmiguel@none.com>
2026-01-10 12:15:45 +0100

    Add MEGA cloud storage driven adapter with file operations support

---
+++ driven-adapters/storage-mega/src/main/kotlin/com/github/taixmiguel/qbs/driven/storage/mega/MegaStorageRepository.kt
@@ -0,0 +1,129 @@
+package com.github.taixmiguel.qbs.driven.storage.mega
+
+import com.github.taixmiguel.qbs.application.port.StorageRepository
+import com.github.taixmiguel.qbs.driven.storage.mega.configuration.Properties
+import dev.carlsen.mega.Mega
+import dev.carlsen.mega.model.Node
+import dev.carlsen.mega.util.CancellationToken
+import dev.carlsen.mega.util.ProgressCountingSink
+import jakarta.enterprise.context.ApplicationScoped
+import jakarta.inject.Inject
+import kotlinx.io.buffered
+import kotlinx.io.files.SystemFileSystem
+import org.eclipse.microprofile.config.Config
+import java.io.File
+import java.nio.file.Path
+import kotlin.io.path.name
+import kotlin.jvm.optionals.getOrElse
+
+@ApplicationScoped
+class MegaStorageRepository: StorageRepository {
+    @Inject
+    private lateinit var config: Config
+
+    @Inject
+    private lateinit var mega: Mega
+
+    override suspend fun push(pathToUpload: Path, file: File) {
+        try {
+            login()
+
+            val node = findNode(pathToUpload, true)
+            val fileToUpload = kotlinx.io.files.Path(file.absolutePath)
+
+            SystemFileSystem.source(fileToUpload).use { fileSource ->
+                mega.uploadFile(
+                    destNode = node!!,
+                    name = file.name,
+                    fileSize = file.length(),
+                    fileInputSource = fileSource.buffered(),
+                    cancellationToken = CancellationToken.default()
+                )
+            }
+        } finally {
+            logout()
+        }
+    }
+
+    override suspend fun pull(path: Path, filename: String): File? {
+        try {
+            login()
+
+            val node = findNode(path, true)
+            val files = mega.getChildren(node!!)
+            val file = files.first { it.name == filename }
+            val fileToDownload = kotlinx.io.files.Path(filename)
+
+            SystemFileSystem.sink(fileToDownload).use { fileOutputSink ->
+                mega.downloadFile(
+                    src = file,
+                    fileOutputSink = ProgressCountingSink(
+                        delegate = fileOutputSink,
+                        totalBytes = file.size,
+                        onProgress = { b, t ->
+                            println("Downloaded $b of $t bytes")
+                        }
+                    ).buffered(),
+                    cancellationToken = CancellationToken.default()
+                )
+            }
+            return File(fileToDownload.toString())
+        } catch (e: Exception) {
+            return null
+        } finally {
+            logout()
+        }
+    }
+
+    override suspend fun remove(path: Path, filename: String) {
+        try {
+            login()
+
+            val node = findNode(path, true)
+            val files = mega.getChildren(node!!)
+            val file = files.first { it.name == filename }
+            mega.delete(file, destroy = true)
+        } catch (e: Exception) {
+            // TODO(not implemented yet)
+        } finally {
+            logout()
+        }
+    }
+
+    private suspend fun login() {
+        val password = config.getValue<String>(Properties.MEGA_PASSWORD, String::class.java)
+        val email = config.getValue<String>(Properties.MEGA_EMAIL, String::class.java)
+        mega.login(email, password)
+    }
+
+    private suspend fun logout() {
+        mega.logout()
+    }
+
+    private suspend fun findNode(path: Path, swCreate: Boolean = false): Node? {
+        val rootNode: Node = mega.getFileSystem().root ?: Node()
+        return findNode(rootNode, path, swCreate)
+    }
+
+    private suspend fun findNode(rootNode: Node, path: Path, swCreate: Boolean = false): Node? {
+        val route = path.toString()
+        val folder = if (route.startsWith("/")) {
+            val index = route.indexOf("/", 1)
+            route.substring(1, if (index < 0) route.length else index)
+        } else route.substring(0, route.indexOf("/"))
+
+        val node = rootNode.getChildren().stream()
+            .filter { it.name == folder }
+            .findFirst()
+            .getOrElse {
+                if (swCreate) mega.createDir(folder, rootNode)
+                else null
+            }
+
+        if (node != null) {
+            val nextPath = Path.of(route.substring(route.indexOf(folder) + folder.length))
+            return if (nextPath.name.isNotEmpty()) findNode(node, nextPath, swCreate) else node
+        }
+        return node
+    }
+}
---
+++ driven-adapters/storage-mega/src/main/kotlin/com/github/taixmiguel/qbs/driven/storage/mega/configuration/Properties.kt
@@ -0,0 +1,6 @@
+package com.github.taixmiguel.qbs.driven.storage.mega.configuration
+
+object Properties {
+    const val MEGA_EMAIL = "qbs.storage.mega.email"
+    const val MEGA_PASSWORD = "qbs.storage.mega.password"
+}
