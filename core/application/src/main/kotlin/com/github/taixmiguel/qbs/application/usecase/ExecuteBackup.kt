68e37a3627ebaefeee4ee092d3c4997e5991d197
TaixMiguel <taixmiguel@none.com>
2026-01-16 16:09:06 +0100

    Add ExecuteBackup use case with directory compression and storage upload

---
+++ core/application/src/main/kotlin/com/github/taixmiguel/qbs/application/usecase/ExecuteBackup.kt
@@ -0,0 +1,47 @@
+package com.github.taixmiguel.qbs.application.usecase
+
+import com.github.taixmiguel.qbs.application.port.StorageRepository
+import com.github.taixmiguel.qbs.domain.Backup
+import java.io.File
+import java.io.FileOutputStream
+import java.nio.file.Path
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.zip.ZipEntry
+import java.util.zip.ZipOutputStream
+
+class ExecuteBackup {
+    suspend fun execute(backup: Backup, repository: StorageRepository) {
+        val now = LocalDateTime.now()
+        val formatter = DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss")
+        val outputPath = "backup_${now.format(formatter)}.bck"
+
+        val backupFile =  File(outputPath)
+        zipDirectory(backup.sourceDir, outputPath)
+        repository.push(backup.destinationDir, backupFile)
+
+        if (backupFile.exists()) backupFile.delete()
+    }
+
+    private fun zipDirectory(sourcePath: Path, outputPath: String) {
+        val sourceDir = File(sourcePath.toString())
+
+        ZipOutputStream(FileOutputStream(outputPath)).use { zipOut ->
+            sourceDir.walkTopDown().forEach { file ->
+                val zipEntryName = file.relativeTo(sourceDir).path
+
+                if (file.isDirectory) {
+                    if (zipEntryName.isNotEmpty()) {
+                        zipOut.putNextEntry(ZipEntry("$zipEntryName/"))
+                        zipOut.closeEntry()
+                    }
+                } else {
+                    val entry = ZipEntry(zipEntryName)
+                    zipOut.putNextEntry(entry)
+                    file.inputStream().use { input -> input.copyTo(zipOut) }
+                    zipOut.closeEntry()
+                }
+            }
+        }
+    }
+}
