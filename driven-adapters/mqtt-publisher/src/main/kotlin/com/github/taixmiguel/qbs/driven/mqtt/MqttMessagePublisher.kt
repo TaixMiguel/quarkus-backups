aeb02233c8bd1b79e6837cf950b0cf1aa759edb4
TaixMiguel <taixmiguel@none.com>
2026-01-23 19:40:45 +0100

    mqtt

--- driven-adapters/mqtt-publisher/src/main/kotlin/com/github/taixmiguel/qbs/driven/mqtt/MqttMessagePublisher.kt
+++ driven-adapters/mqtt-publisher/src/main/kotlin/com/github/taixmiguel/qbs/driven/mqtt/MqttMessagePublisher.kt
@@ -1,6 +1,7 @@
 package com.github.taixmiguel.qbs.driven.mqtt
 
 import com.github.taixmiguel.qbs.application.port.publisher.MessagePublisher
+import io.netty.handler.codec.mqtt.MqttQoS
 import org.eclipse.microprofile.reactive.messaging.Channel
 import org.eclipse.microprofile.reactive.messaging.Emitter
 import io.smallrye.reactive.messaging.mqtt.MqttMessage
@@ -9,10 +10,10 @@ import jakarta.inject.Inject
 
 @ApplicationScoped
 class MqttMessagePublisher @Inject constructor(
-    @Channel("backup-status") private val emitter: Emitter<String>
+    @field:Channel("backup-status") private val emitter: Emitter<MqttMessage<String>>
 ): MessagePublisher {
     override fun publish(topic: String, payload: String, retain: Boolean) {
-        val message: MqttMessage<String> = MqttMessage.of(topic, payload)
+        val message: MqttMessage<String> = MqttMessage.of(topic, payload, MqttQoS.AT_MOST_ONCE, retain)
         emitter.send(message)
     }
 }
